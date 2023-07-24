6f27473eb6aed63984dc09dfaa99f75882b3ac5d
TheSciPro <104713732+TheSciPro@users.noreply.github.com>
2023-07-24 07:18:36 +0530

    Create array.cpp
    initialize entire array with single value

---
+++ lec9/array.cpp
@@ -0,0 +1,22 @@
+int main(void)
+{
+    int size = 6;
+    int val = 1;
+  
+    // array declaration
+    int arr[size];
+    int i;
+  
+    // initializing array elements
+    for (i = 0; i < size ; i++){
+        arr[i] = val;
+    }
+  
+     // print array
+     printf("The array is:");
+     for (i = 0; i < size ; i++){
+         printf("%d ", arr[i]);
+    }
+  
+    return 0;
+}
